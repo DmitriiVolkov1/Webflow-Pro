390fe2d485faa719ebf7334d20378555b17de79a
Дмитрий Волков <you@example.com>
2024-03-15 13:38:04 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+console. log("Заебись")
+
+const firstName = "Dmitrii"
+console. log ("My name", firstName)
+
+const age = "32"
+console. log ("My age", age)
+
+const city = "N"
+console. log ("My city", city)
+
+const about = "Webflow developer"
+console. log ("About me", about)
