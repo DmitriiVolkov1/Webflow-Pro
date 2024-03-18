0acc2eb08474a0e65a3554d0445e0c9eeac7320d
Дмитрий Волков <you@example.com>
2024-03-18 17:53:08 +0300

    Lesson 03

---
+++ Lesson 03/app03.js
@@ -0,0 +1,15 @@
+
+
+let titleProject = 'Main Project'
+
+let screensValue = 'шаблонные, с уникальным дизайном, с анимациями'
+
+let screenPrice = 1000
+
+let percentage = 10
+
+let fullPrice = 1000000
+
+let responsive = true
+
+console.log (titleProject);
